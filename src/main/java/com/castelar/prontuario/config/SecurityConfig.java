dee4e990524e92a0fa6562d80c546ada5acaca74
BKluuis <luisscalabrin@gmail.com>
2023-11-16 22:46:51 -0300

    fix(SECURITY)!: Adicionado configuração de compilação Maven
    Consertado erro "This method cannot decide whether these patterns are Spring MVC patterns or not. If this endpoint is a Spring MVC endpoint, please use requestMatchers(MvcRequestMatcher); otherwise, please use requestMatchers(AntPathRequestMatcher)."

--- src/main/java/com/castelar/prontuario/config/SecurityConfig.java
+++ src/main/java/com/castelar/prontuario/config/SecurityConfig.java
@@ -2,19 +2,25 @@ package com.castelar.prontuario.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableWebMvc
 public class SecurityConfig {
+    @Bean
+    public MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector){
+        return new MvcRequestMatcher.Builder(introspector);
+    }
+
     /**
      * 
      * @param http
@@ -22,10 +28,10 @@ public class SecurityConfig {
      * @throws Exception
      */
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
             .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .authorizeHttpRequests((requests) -> requests.requestMatchers(HttpMethod.POST, "/login", "/register").permitAll()
+            .authorizeHttpRequests((requests) -> requests.requestMatchers(mvc.pattern("/login"), mvc.pattern("/register")).permitAll()
             .anyRequest().authenticated()
         );
         return http.build();
